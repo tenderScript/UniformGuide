a4673b175fc4a3c97643cbf6edc04f27c20dd229
Brian Scaturro <scaturrob@gmail.com>
2014-11-20 15:11:22 -0500

    form module

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,29 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
+    bourbon = require('node-bourbon'),
     del = require('del');
 
-var srcDir = "src",
+var srcDir = "assets",
     distDir = "dist",
     watchPaths = ['scss'].reduce(function(paths, ext) {
       return paths.concat([srcDir + '/**/*.' + ext, srcDir + '/*.' + ext]);
     }, []),
-    sassConfig = {};
+    sassConfig = {
+      includePaths: bourbon.includePaths
+    };
 
 gulp.task('clean', function(cb) {
   del([distDir], cb);
 });
 
 gulp.task('sass', ['clean'], function() {
-  gulp.src([srcDir + '/main.scss'])
+  gulp.src([srcDir + '/stylesheets/uniform.scss'])
       .pipe(sass(sassConfig))
       .pipe(gulp.dest(distDir))
 });
 
 gulp.task('copy-fonts', ['clean'], function() {
-  gulp.src('bower_components/bootstrap-sass-official/assets/fonts/**/*.{ttf,woff,svg,eot}')
+  gulp.src('assets/fonts/*.{ttf,woff,svg,eot}')
       .pipe(gulp.dest(distDir + '/fonts'));
 });
 
@@ -29,5 +32,5 @@ gulp.task('dev', ['build'], function() {
 });
 
 gulp.task('copy', ['copy-fonts']);
-gulp.task('build', ['webpack', 'sass', 'copy']);
+gulp.task('build', ['sass', 'copy']);
 gulp.task('default', ['build']);
