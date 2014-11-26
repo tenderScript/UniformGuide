8d4a9358ad819405ba5d9ad2ce3d35c1d9b5505f
Brian Scaturro <scaturrob@gmail.com>
2014-11-26 13:17:53 -0500

    fix variable typo

--- assets/js/directives/logout-button/logout-button.js
+++ assets/js/directives/logout-button/logout-button.js
@@ -20,7 +20,7 @@
         args = [this.$scope.cookie],
         redirect = (function(path) {
           return function() {
-            that.$window.location = $path;
+            that.$window.location = path;
           };
         })(this.$scope.redirect);
     this.$scope.cookieDomain != '*' && (args = args.concat([{domain: this.$scope.cookieDomain}]));
